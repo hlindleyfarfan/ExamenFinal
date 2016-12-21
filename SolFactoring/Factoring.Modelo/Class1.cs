c48c55e756ec6a98aefda130583daee75a807331
hlindley <hector.lindley@gmail.com>
2016-12-20 19:59:03 -0500

    git hub 1

---
+++ SolFactoring/Factoring.Modelo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factoring.Modelo
+{
+    public class Class1
+    {
+
+        //prueba github
+
+    }
+}
